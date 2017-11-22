83e5d61bf2a7b910fefacd6211e5e11e5953c30a
frogtube <u.pradere@gmail.com>
2017-11-22 11:36:39 +0100

    Creating Video entity

---
+++ src/AppBundle/Entity/Video.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ugo-fixe
+ * Date: 22/11/2017
+ * Time: 10:57
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="video")
+ */
+class Video
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $embed;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $caption;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmbed()
+    {
+        return $this->embed;
+    }
+
+    /**
+     * @param mixed $embed
+     */
+    public function setEmbed($embed)
+    {
+        $this->embed = $embed;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCaption()
+    {
+        return $this->caption;
+    }
+
+    /**
+     * @param mixed $caption
+     */
+    public function setCaption($caption)
+    {
+        $this->caption = $caption;
+    }
+
+}
